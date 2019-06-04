d40a3fb602c543d839dd16518cdd183d5d16154b
nysand <46599021+nysand@users.noreply.github.com>
2019-06-04 15:54:33 +0100

    Add files via upload

---
+++ Shared/AzureHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Model
+{
+    public class AzureHelper
+    {
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://deliveryxamdemo1.azurewebsites.net/");
+
+        public static async Task<bool> Insert<T>(T objectToInsert)
+        {
+
+            try
+            {
+                await MobileService.GetTable<T>().InsertAsync(objectToInsert);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Shared/User.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+
+namespace DeliveryApp.Model
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public static async Task<bool> Login(string email, string password)
+        {
+            bool result = false;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                result = false;
+            }
+            else
+            {
+                var user = (await AzureHelper.MobileService.GetTable<User>().Where(u => u.Email == email).ToListAsync()).FirstOrDefault();
+
+                if (user.Password == password)
+                {
+                    result = true;
+                }
+
+                else
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        public static async Task<bool> Register(string email, string password, string confirmPassword)
+        {
+            bool result = false;
+
+            if (!string.IsNullOrEmpty(password))
+            {
+                if (password == confirmPassword)
+                {
+                    var user = new User()
+                    {
+                        Email = email,
+                        Password = password
+                    };
+
+                    await AzureHelper.Insert(user);
+
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
